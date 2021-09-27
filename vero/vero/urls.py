29219d76439afbbd0bd00bc9e3499d8dcd2842c6
JessicaParrado <jessica.parrado01@correo.usa.edu.co>
2021-09-27 18:56:00 -0500

    Implementación Landing con Django

--- vero/vero/urls.py
+++ vero/vero/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('home.urls')),
     path('personalActivities/', include('personalActivities.urls')),
+    path('ejemplo/', include('ejemplo.urls')),
     path('admin/', admin.site.urls),
 ]
