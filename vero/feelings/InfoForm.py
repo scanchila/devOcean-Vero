9b94c2b0e1a430ac993112177e9d5e28070064ea
alejandro <luis.vejarano01@correo.usa.edu.co>
2021-09-29 23:42:00 -0500

    .

--- vero/feelings/InfoForm.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import get_object_or_404
-from .forms import HowDoYouFeel
-
-def getInfoForm(request):
-    formInst = get_object_or_404()
--- vero/feelings/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeelingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'feelings'
