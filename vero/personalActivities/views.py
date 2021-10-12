93fa9f08e6d762fa222dced03a9b957dcd33211c
alejandro <luis.vejarano01@correo.usa.edu.co>
2021-10-12 10:42:16 -0500

    Corrección views

--- vero/personalActivities/views.py
+++ vero/personalActivities/views.py
@@ -2,18 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import JsonResponse
-from .forms import *
 from django.views.decorators.csrf import csrf_exempt
-import json
 
 # Create your views here.
 
 def main(request):
-  context = {}
-  context['form'] = personalActivitiesForm()
-  if request.method == 'POST':
-    print(request.POST)
-  return render(request,"personalActivities/filtroActividadesIndividuales.html", context)
+  return render(request,"personalActivities/filtroActividadesIndividuales.html")
 
 @csrf_exempt
 def actividad(request):
