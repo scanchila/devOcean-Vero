c345d33d5407ddc90206a2a089351f22d1eb556c
SebastianSierrajc <sebastian.sierrajc@gmail.com>
2021-10-13 16:01:00 -0500

    acutalizacion modelo actividades individuales

--- vero/personalActivities/admin.py
+++ vero/personalActivities/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ActivityType, PersonalActivites
+from .models import ActivityType, PersonalActivites, ActivityCategory
 # Register your models here.
 
 admin.site.register(ActivityType)
 admin.site.register(PersonalActivites)
+admin.site.register(ActivityCategory)
 
--- vero/personalActivities/models.py
+++ vero/personalActivities/models.py
@@ -6,21 +6,30 @@ from django.utils import timezone
 
 # Create your models here.
 class ActivityType(models.Model):
-  name = models.CharField(max_length=255)
-  description = models.CharField(max_length=255)
+  name = models.CharField(max_length=254, unique=True)
+  description = models.CharField(max_length=254)
 
   def __str__(self):
     return self.name
 
+
+class ActivityCategory(models.Model):
+  name = models.CharField(max_length=254, unique=True)
+  description = models.CharField(max_length=254)
+
+  def __str__(self) -> str:
+    return self.name
+
 class PersonalActivites(models.Model):
-  name = models.CharField(max_length=255)
-  description = models.CharField(max_length=255)
-  imageURL = models.CharField(max_length=255)
-  videoURL = models.CharField(max_length=255)
-  lecture = models.CharField(max_length=255)
+  name = models.CharField(max_length=254, unique=True)
+  description = models.CharField(max_length=254)
+  image_URL = models.URLField(max_length=1000, blank=True)
+  video_URL = models.URLField(max_length=1000, blank=True)
+  lecture = models.CharField(max_length=1000000, blank=True)
   pub_data = models.DateTimeField('date published')
-  duration = models.DurationField(max_length=255)
+  duration = models.DurationField()
   activityType = models.ForeignKey(ActivityType, on_delete=models.CASCADE)
+  categories = models.ManyToManyField(ActivityCategory)
 
   def wasPublishedRecently(self):
     return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
