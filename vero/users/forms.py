d86fa9f88a0deb674d3bbd73db1c124e00270da7
SebastianSierrajc <sebastian.sierrajc@gmail.com>
2021-10-23 19:44:06 -0500

    creacion perfil usuario cuando el usuario se registra

--- vero/users/forms.py
+++ vero/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User_profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,6 +10,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        up = User_profile(user=user)
+        if commit:
+            user.save()
+            up.save()
+        return user
+
 
 class UserLoginForm(AuthenticationForm):
     pass
