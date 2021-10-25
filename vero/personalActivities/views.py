7f0034b24b0c04cbeaaafe40b3c736c24b0723b3
alejandro <luis.vejarano01@correo.usa.edu.co>
2021-10-25 14:18:37 -0500

    Agrego filtro completo por tipo

--- vero/personalActivities/views.py
+++ vero/personalActivities/views.py
@@ -13,7 +13,7 @@ from .models import ActivityType, ActivityCategory, PersonalActivites
 @login_required(login_url='/users/login/')
 def main(request):
   all_activities = PersonalActivites.objects.order_by('-pub_data')
-  act_name = ActivityCategory.objects.all()
+  act_name = ActivityType.objects.all()
   context = {
     "pageTitle": "Personal Activities list",
     "activities": all_activities,
@@ -31,13 +31,29 @@ def singleActivity(request, activity_id):
   }
   return render(request, "personalActivities/actividad.html", context)
 
-
+@login_required(login_url='/users/login/')
 def recibirActividad(request):
-  if request.method == "POST" and request.is_ajax():
-    #for x in request.POST:
-    #  print(request.POST[x])
-    return JsonResponse({"success": True, "respuesta": "siuu"}, status=200)
-  return JsonResponse({"success": False, "respuesta": "noou"}, status=400)
+  context = {}
+  if request.method == "POST":
+    activities = None
+    if request.POST["resource"] == "any" and request.POST["time"] == "any" and request.POST["act_type"] == "any":
+      activities = PersonalActivites.objects.order_by('-pub_data')
+    elif request.POST["resource"] == "any" and request.POST["time"] == "any" and request.POST["act_type"] != "any":
+      act_type = request.POST["act_type"]
+      activities = PersonalActivites.objects.raw("select * from personalActivities_personalactivites "
+                                                 "where (select id from personalActivities_activitytype "
+                                                 f"where name = '{act_type}') = "
+                                                 "activityType_id")
+
+    act_name = ActivityType.objects.all()
+    context = {
+      "pageTitle": "Personal Activities list",
+      "activities": activities,
+      "act_name": act_name
+    }
+
+  return render(request, "personalActivities/filtroActividadesIndividuales.html", context)
+
 
 
 
