2f0d4e35970f3cb9ba16c427b812e1cb9fa81c8a
Santiago <santiago.gutierrez02@correo.usa.edu.co>
2021-11-08 18:09:45 -0600

    Correción errores

--- vero/grupalActivities/urls.py
+++ vero/grupalActivities/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('grupalActivities/', views.index, name='grupalActivities'),
     path('myActivities/', views.myactivity, name='myActivities'),
     path('filtroActividadesgrupales/', views.grupal, name='filtroActividadesgrupales'),
-    path('recibirActividadGrupal/', views.recibirActividadGrupal, name='recibirActividadGrupal'),
-    path('grupalActivity_selection/<int:activity_id>/',
-         views.grupalActivity_selection, name='grupal_activity_selection')
+    path('recibirActividadGrupal/', views.recibirActividadGrupal, name='recibirActividadGrupal')
 ]
