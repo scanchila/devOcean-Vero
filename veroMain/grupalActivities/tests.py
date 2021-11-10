14b4afa952f339650a6987f8427f5e8aeb488efa
LadySalazar <lady.salazar01@correo.usa.edu.co>
2021-11-09 21:15:40 -0500

    testeo filtro actividad grupal

--- veroMain/grupalActivities/tests.py
+++ veroMain/grupalActivities/tests.py
@@ -1,14 +1,92 @@
 from django.test import TestCase
-from .models import GroupActivity
-from django.contrib.auth.models import User
-# Create your tests here.
-class GroupActivityTestCase(TestCase):
-    def setUp(self):
-        user = User(username="TestGroupActivity", email="test@test.com",password="Test123")
-        user.save()
-        GroupActivity.objects.create(name="Test",address = "CRA Test # 44", contact="test@test.com",
-                                     date = "2021-10-24", hour="09:50PM", duration="10", type="test",
-                                     creator=user, description="This is a test")
 
-    def test_exist(self):
-        g = GroupActivity.objects.get(name="Test")
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support.ui import Select
+# Create your tests here.
+class ActivityGrupal(StaticLiveServerTestCase):
+  
+  
+  def testFilterActivity(self):
+    driver = webdriver.Chrome(executable_path="C:\\ll\\chromedriver.exe")
+    print(self.live_server_url)
+    driver.get(self.live_server_url+'/users/register/')
+    username = driver.find_element_by_id('id_username')
+    password1 = driver.find_element_by_id('id_password1')
+    password2 =driver.find_element_by_id('id_password2')
+    email = driver.find_element_by_id('id_email')
+    submit = driver.find_element_by_id('Registrar')
+    terminos = driver.find_element_by_name('terminos')
+
+
+    username.send_keys('TestUser1')
+    password1.send_keys('Testpassword123')
+    email.send_keys('test@test.com')
+    terminos.click()
+    password2.send_keys('Testpassword123')
+    time.sleep(1)
+    submit.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+
+    username = driver.find_element_by_id('id_username')
+    password = driver.find_element_by_id('id_password')
+
+    password.send_keys('Testpassword123')
+    username.send_keys('TestUser1')
+    submit = driver.find_element_by_id('Registrar')
+    submit.click()
+
+    time.sleep(2)
+
+    driver.get(self.live_server_url+'/myActivities/grupalActivities/')
+    activitiName=driver.find_element_by_name('nombre')
+    activitiType=Select(driver.find_element_by_id('tipo'))
+    activitiDescription=driver.find_element_by_name('descripcion')
+    activitiDuration=driver.find_element_by_name('duracion')
+    activitiDate=driver.find_element_by_name('fecha')
+    activitihour=driver.find_element_by_name('hora')
+    activitimax=driver.find_element_by_name('maximo')
+    activitiaddres=driver.find_element_by_name('direccion')
+    activitiemail=driver.find_element_by_name('email')
+    submit2 = driver.find_element_by_id('SubmitCreate')
+
+
+
+    activitiName.send_keys('Danza Forclorica')
+    
+    activitiType.select_by_visible_text('folclor')
+    
+    activitiDescription.send_keys('Danza muy divertida')
+    
+    activitiDuration.send_keys('00:30:00')
+    
+    activitiDate.send_keys('2021-12-12')
+    
+    activitihour.send_keys('07:02a. m.')
+    
+    activitimax.send_keys('10')
+    
+    activitiaddres.send_keys('Carrera 60')
+    
+    activitiemail.send_keys('pepito@perez.com')
+    
+    submit2.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    driver.get(self.live_server_url+'/myActivities/filtroActividadesgrupales/')
+    
+    select = Select(driver.find_element_by_name('time'))
+    submit = driver.find_element_by_id('btnFiltro')
+    
+    select.select_by_value('any')
+    time.sleep(1)
+    submit.send_keys(Keys.RETURN)
+    
+
+
+    assert 'Actividades Grupales' in driver.page_source
